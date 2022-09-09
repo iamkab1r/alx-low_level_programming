9fca0170d47ee1115d4965388c408998b1ac3d03
Kabiru <adermkabeer@gmail.com>
2022-09-09 12:32:06 -0700

    C Commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,14 +3,14 @@
  * main - main block
  * Description: Print all numbers of base 10, starting from 0.
  * Return: 0
- * */
+ */
 int main(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		printf ("%i", i);
+		printf("%i", i);
 	}
 	putchar('\n');
 
