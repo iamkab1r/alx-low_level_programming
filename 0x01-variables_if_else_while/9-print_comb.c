51f5cfbafe48aff54f597db39764d9ba98807e6e
Kabiru <adermkabeer@gmail.com>
2022-09-09 12:43:13 -0700

    C Commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -17,12 +17,12 @@ int main(void)
 		putchar(i + '0');
 		if (i < 9)
 		{
-			putchar(44)
+			putchar(44);
 			putchar(32);
 		}
-		i++;
+		i++;							
 	}
-	putchar('\n');
-	
+	putchar('\n');		
+
 	return (0);
 }
