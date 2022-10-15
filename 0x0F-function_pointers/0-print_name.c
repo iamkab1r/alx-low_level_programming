1a4ed6c0536e632170ec56e7323c5ec748a58a12
Kabir Umar <111059385+oakdevspace@users.noreply.github.com>
2022-10-15 02:52:24 +0100

    execute

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,12 @@
-#include "function_pointers.h"
-
 /**
- * print_name - prints a name
- * @name: string
- * @f: function
- */
-
+  *print_name - prints a name.
+  *@name: pointer to name.
+  *@f: function pointer.
+  *
+  *Return: void.
+  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (f != NULL)
+	if (name && f)
 		f(name);
 }
